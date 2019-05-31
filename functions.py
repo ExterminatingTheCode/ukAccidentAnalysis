d6faddb995b14b279e36284fda8e63184274056a
mac <mac@macs-MacBook-Air.local>
2019-05-31 09:27:43 -0700

    Incorporated functions, cleaned up code in the hypothesis testing notebooks

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
-def load_and_clean_accident_data():
+import pandas as pd
+import numpy as np
+from scipy import stats
 
+
+def load_and_clean_accident_data():
     df1 = pd.read_csv("data/accidents_2005_to_2007.csv")
     df2 = pd.read_csv("data/accidents_2009_to_2011.csv")
     df3 = pd.read_csv("data/accidents_2012_to_2014.csv")
@@ -22,4 +26,34 @@ def load_and_clean_accident_data():
     for col in unnecessary_columns:
         del df[col]
 
+    return df
 
+def find_severity_mus(df):
+    severity_totals = df.groupby('Accident_Severity').size().values
+    #total is 1,469,963 number of accidents 1469963
+    severity_mus = severity_totals / severity_totals.sum()
+    return severity_mus
+
+
+# This function is no longer used because I realized dot product is better
+def calculate_expected_severities(severity_mus,corr_sums):
+    n_mus = len(severity_mus)
+
+    n_cats = len(corr_sums)
+    print(n_mus, " ",n_cats)
+    expected = np.zeros((n_cats,n_mus))
+    for i in np.arange(n_mus):
+        for j in np.arange(n_cats):
+            expected[i][j] = severity_mus[j] * corr_sums[i]
+    expected = expected.round().astype(int)
+    return expected
+
+def calculate_p_values(actual_severity,expected_severity):
+    n_corr, _ = expected_severity.shape
+    pvalues = np.zeros(n_corr)
+
+    for i in np.arange(n_corr):
+        pvalues[i] = stats.chisquare(actual_severity[i],expected_severity[i])[1]
+    pvalues = pvalues.reshape((-1,1))
+    
+    return pvalues
